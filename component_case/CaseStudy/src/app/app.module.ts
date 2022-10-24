49f908a03845bea14f1c126c64f5130d92fe041e
NgocHuy98 <huydn2101@gmail.com>
2022-10-24 23:22:37 +0700

    case sudty

--- component_case/CaseStudy/src/app/app.module.ts
+++ component_case/CaseStudy/src/app/app.module.ts
@@ -5,16 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { ContractComponent } from './contract/contract.component';
 import { NavComponent } from './nav/nav.component';
-import { CreateContractComponent } from './create-contract/create-contract.component';
-import { FacilityModule } from './facility.module';
 import {ListFacilityComponent} from './facility/list-facility/list-facility.component';
 import {EditFacilityComponent} from './facility/edit-facility/edit-facility.component';
 import {CreateFacilityComponent} from './facility/create-facility/create-facility.component';
 import {EditCustomerComponent} from './customer/edit-customer/edit-customer.component';
 import {CreateCustomerComponent} from './customer/create-customer/create-customer.component';
 import {ListCustomerComponent} from './customer/list-customer/list-customer.component';
+import { ListContractComponent } from './contract/list-contract/list-contract.component';
+import {CreateContractComponent} from './contract/create-contract/create-contract.component';
 
 @NgModule({
   declarations: [
@@ -24,17 +23,17 @@ import {ListCustomerComponent} from './customer/list-customer/list-customer.comp
     HomeComponent,
     ListCustomerComponent,
     ListFacilityComponent,
-    ContractComponent,
     NavComponent,
     EditFacilityComponent,
     CreateFacilityComponent,
     EditCustomerComponent,
     CreateCustomerComponent,
-    CreateContractComponent
+    CreateContractComponent,
+    ListContractComponent
   ],
   imports: [
-    BrowserModule,
-    FacilityModule
+    BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- component_case/CaseStudy/src/app/service/facility-service.service.ts
+++ component_case/CaseStudy/src/app/service/facility-service.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Facility} from '../facility';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacilityServiceService {
 
-  constructor() { }
+  facilityList = new Array();
+
+  constructor() {
+    this.facilityList.push(new Facility(1, 'PenHouse Đà Lạt', 100, 2000, 2, 3, 2, 'vip', 'có tủ lạnh', 100, 1, 'Không'));
+    this.facilityList.push(new Facility(2, 'BathRoom Lakonku', 100, 3000, 2, 2, 3, 'vip', 'có tủ lạnh', 100, 1, 'Không'));
+    this.facilityList.push(new Facility(3, 'Mandar Palace', 300, 7000, 10, 1, 1, 'vip', 'đầy đủ', 100, 3, 'Không'));
+    this.facilityList.push(new Facility(4, 'Murharang Kingdom', 500, 10000, 20, 1, 1, 'vip', 'đầy đủ', 100, 2, 'Không'));
+    this.facilityList.push(new Facility(5, 'Brenfodd Villa', 200, 5000, 5, 3, 1, 'vip', 'đầy đủ', 100, 2, 'Không'));
+    this.facilityList.push(new Facility(6, 'Alexander Villa', 200, 5000, 5, 3, 1, 'vip', 'đầy đủ', 100, 2, 'Không'));
+  }
+
+  findAll(): Facility[] {
+    return this.facilityList;
+  }
 }
